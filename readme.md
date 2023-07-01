166509cc54923a80d970478bef6e2074d9ef99d1
Samet Akcabay <samet.akcabay@outlook.com>
2023-07-01 13:21:28 +0200

    commit current progress

--- readme.md
+++ readme.md
@@ -1,2 +1,23 @@
-# MasterThesis
-This repo contains all files related to my Master's thesis.
+# Entwicklung eines interaktiven Werkzeugs zur Kuratierung von Umweltdaten einer bürgerinitiierten Crowdsensing-Kampagne
+
+![MOBI-Logo](/misc/mobi_logo.png "MOBI-Logo")
+
+In dieser Arbeit soll es darum gehen, den Bürgerinnen und Bürgern der Stadt Bamberg die Möglichkeit zu bieten, die Sensordaten des Klimamessnetzvereins Bamberg (BVM) einzusehen und durch die Funktion des Hinzufügens von Annotationen zu diesen, den Crowdsensing-Aspekt näher zu bringen. Dadurch ergeben sich die Leitfragen, inwieweit sich dies auf die Entwicklung eines solchen Tools auswirkt, inwiefern ein Ansatz wie dieser sinnvoll ist und in der Zukunft berücksichtigt werden sollte.
+
+## Technische Umsetzung
+
+- Python 3.11 als Grundwerkzeug zum Einbinden diverser, benötigter Frameworks
+- Flask als Implementierung der Webapplikation (zunächst als Webserver)
+- Jinja als Template-Engine für Flask
+- SQLite als Datenbank zum Speichern von Annotationen, Sensordaten, Credentials und Feedback
+
+## Stakeholder
+
+- Klimamessnetz Bamberg (BVM)
+- Prof. Thomas Foken
+- Projektgruppe des Lehrstuhls für Visualisierung von Fabian Beck (Mitglieder: Pascal Löffler und Florian Aschinger)
+- ...
+
+## Anforderungen
+
+tba.
--- src/main.py
+++ src/main.py
@@ -6,60 +6,25 @@
 * Annotationen
 * Feedback?
 * Visualisierung einbinden
+* DB wie umsetzen?
 '''
 
 '''
 # NOTE:
 * Flask als Framework für Webapp mit Python (https://learn.microsoft.com/de-de/visualstudio/ide/quickstart-python?view=vs-2022)
 * Python 3.11 als Python-Version, requirements.txt für Frameworks pflegen
+* Jinja als Template-Engine
 # '''
 
 from flask import Flask
-from flask import url_for
-from markupsafe import escape
+from flask import render_template
 
-# Create an instance of the Flask class that is the WSGI application.
-# The first argument is the name of the application module or package,
-# typically __name__ when using a single module.
+# create an instance of class Flask
 app = Flask(__name__)
 
-# Flask route decorators map / and /hello to the hello function.
-# To add other resources, create functions that generate the page contents
-# and add decorators to define the appropriate resource locators for them.
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('login.html', name=name)
 
-@app.route('/')
-def home():
-    return "This is the homepage."
-
-@app.route('/login')
-def login():
-    return "Here is the login page"
-
-@app.route('/index')
-def hello():
-    # Render the page
-    return "Hello Python!"
-
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
-
-with app.test_request_context():
-    # print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
-if __name__ == '__main__':
-    # Run the app server on localhost:4449
-    app.run('localhost', 4449)
+# run application with command "flask --app <name_of_pythonfile without .py> run"
