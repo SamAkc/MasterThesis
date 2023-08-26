2e86813442883d5802eeed1e0e0b323ab56aa677
Samet Akcabay <samet.akcabay@outlook.com>
2023-08-26 14:59:56 +0200

    current state

---
+++ src/index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('map.html')
+
+@app.route("/feedback")
+def feedback():
+    return render_template('feedback.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
